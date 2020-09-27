d0a285178ae6c73a02a8931ab320fdf91386fa10
Collin Blake <collinblake1@gmail.com>
2020-09-26 21:00:26 -0600

    02 Teaching Activity

--- web/assignment.php
+++ web/assignment.php
@@ -14,8 +14,9 @@
 		<main>
             <ul>
                 <li><h2>Assignment Links</h2></li>
-                <li><a href="./index.php">PHPinfo Page</a></li>
-                <li><a href="./hello.html">Hello World Page</a></li>                
+                <li><a href="./index.php">W01 - PHPinfo Page</a></li>
+                <li><a href="./hello.html">W01 - Hello World Page</a></li>
+                <li><a href="./02Teach/teach2.html">W02 - 02 Teach: Team Activity</a></li>
                 <!-- <li><a href=""></a></li> Template for later additions -->
             </ul>
 		</main>
