0a65eb605184b3787971b5a39dc4df94c9af409b
Collin Blake <collinblake1@gmail.com>
2020-10-09 22:08:43 -0600

    Missing Comma

--- web/Assignments/DBAccess/index.php
+++ web/Assignments/DBAccess/index.php
@@ -17,11 +17,11 @@
         <h2>Game Index</h2>
         <div id="gridContainer">        
             <?php                
-                foreach ($db->query('SELECT g.game_id g.name, i.img_name, i.alt_txt FROM games g INNER JOIN images i ON g.game_id = i.game_id ') as $row)
+                foreach ($db->query('SELECT g.game_id, g.name, i.img_name, i.alt_txt FROM games g INNER JOIN images i ON g.game_id = i.game_id') as $row)
                 {
-                  echo "<a href='./details.php?id={$row['game_id']}' class='gameItem'><p>{$row['name']}</p>
+                  echo "<div><a href='./details.php?id={$row['game_id']}' class='gameItem'><p>{$row['name']}</p>
                   <img src='./imgs/{$row['img_name']}' alt='{$row['alt_txt']}'>
-                  <a>";
+                  <a></div>";
                 }
             ?>                   
         </div>
