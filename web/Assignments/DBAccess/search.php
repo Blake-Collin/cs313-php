3e9c0278b60bde4f80d074918de3fd40b2254c1a
Collin Blake <collinblake1@gmail.com>
2020-10-12 18:12:22 -0600

    Search Function

--- web/Assignments/DBAccess/search.php
+++ web/Assignments/DBAccess/search.php
@@ -2,8 +2,25 @@
 
 <?php 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        
+        if (empty($_POST["game"])) {
+            $gameErr = "Game name is required";
+        } else {
+            $name = test_input($_POST["game"]);
+            if (!preg_match("/^[a-zA-Z-' ]*$/",$game)) {
+                $gameErr = "Only letters and white space allowed";
+            }
+        }
+
+
     }
+
+    //Function for clearing inputs for code injections
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
 ?>
 
 <!DOCTYPE html>
@@ -21,17 +38,21 @@
 
     <main>
         <h2>Search Games</h2>
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-            <input type="text" name="game">
+        <form id="searchForm" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <label>Name: <input type="text" name="game" value="<?php echo $game ?>">
+            <span class="error">* <?php echo $nameErr;?></span></label>
             <input type="submit" value="Search">
         </form>        
         <div id="gridContainer">        
             <?php                
-                foreach ($db->query('SELECT g.game_id, g.name, i.img_name, i.alt_txt FROM games g INNER JOIN images i ON g.game_id = i.game_id') as $row)
+                if($gameErr == "" && $game != "")
                 {
-                  echo "<div><a href='./details.php?id={$row['game_id']}' class='gameItem'><p>{$row['name']}</p>
-                  <img src='./imgs/{$row['img_name']}' alt='{$row['alt_txt']}'>
-                  <a></div>";
+                    foreach ($db->query("SELECT g.game_id, g.name, i.img_name, i.alt_txt FROM games g INNER JOIN images i ON g.game_id = i.game_id WHERE g.name LIKE '%$game%'") as $row)
+                    {
+                        echo "<div><a href='./details.php?id={$row['game_id']}' class='gameItem'><p>{$row['name']}</p>
+                        <img src='./imgs/{$row['img_name']}' alt='{$row['alt_txt']}'>
+                        <a></div>";
+                    }
                 }
             ?>                   
         </div>
