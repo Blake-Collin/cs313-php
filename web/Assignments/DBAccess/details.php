d2a6f75ff5d5671578156722f6e965d7b4431a2f
Collin Blake <collinblake1@gmail.com>
2020-10-12 23:51:30 -0600

    Added PHP forms to details page for later

--- web/Assignments/DBAccess/details.php
+++ web/Assignments/DBAccess/details.php
@@ -31,8 +31,89 @@ if(isset($_GET['ID']))
         {
             $details = $rows->fetch(PDO::FETCH_ASSOC);
         }
-
 }
+
+$condition = $textbox = $rate = "";
+$price = 0.00;
+$conditionErr = $reivewErr = $rateErr = $priceErr = "";
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if($_POST["action"] == "review")
+    {
+        if (empty($_POST["textbox"])) {
+            $reivewErr = "Review Text is required!";
+        } else {
+            $textbox = test_input($_POST["textbox"]);
+        }
+
+        if (empty($_POST["rate"]))
+        {
+            $rateErr = "Rating cannot be empty";
+        } else {
+            $rate = intval(test_input($_POST["rate"]));
+            if($rate < 1 || $rate > 5)
+            {
+                $rateErr = "Rating must be between 1 and 5!";
+            }
+        }
+
+
+        if($reivewErr == "" 
+            && $textbox != ""
+            && $rateErr == "")
+            {
+                //Do insert here into sales table later
+            }
+
+
+    }
+    else if($_POST["action"] == "sell")
+    {
+        if (empty($_POST["price"])) {
+            $priceErr = "Price is required";
+        } else {
+            if(!preg_match("/^([1-9][0-9]*|0)(\.[0-9]{2})?$/", test_input($_POST["price"])))
+            {
+                $priceErr = "Price is not in the correct format or positive.";
+            }
+            else
+            {
+                $price = floatval(test_input($_POST["price"]));
+            }            
+        }
+
+        if(empty($_POST["condition"]))
+        {
+            $conditionErr = "Condition Cannot be blan!k";
+        }
+        else
+        {
+            $condition = test_input($_POST["condition"]);
+            if (!preg_match("/^[a-zA-Z ]*$/",$condition)) {
+                $stateErr = "Only letters and white space allowed!";
+              }
+        }
+
+        if($condition != "" &&
+            $conditionErr == "" &&
+            $priceErr == "")
+            {
+                //Add statement here to insert into our array later
+            }
+    }    
+}
+
+
+
+    //Function for clearing inputs for code injections
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
 
 ?>
 
@@ -78,15 +159,59 @@ if(isset($_GET['ID']))
                 <div>
                     <p>Coming Soon!</P>
                 </div>
-                <!--- Needs to be built out later -->
+                <div>
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                        <label> Review: 
+                            <span>
+                                <select name="condition" value="<?php echo $condition;?>">
+                                    <option value='New'>New</option>
+                                    <option value='Like New'>Like New</option>
+                                    <option value='Very Good'>Very Good</option>
+                                    <option value='Good'>Good</option>
+                                    <option value='Acceptable'>Acceptable</option>
+                                    <option value='Unacceptable'>Unacceptable</option>
+                                </select>
+                                <span class="error">* <?php echo $conditionErr;?></span>
+                            </span>
+                        </label>
+                        <label> Price: 
+                            <span>
+                                <input type="text" name="price" value="<?php echo $price;?>">
+                                <span class="error">* <?php echo $priceErr;?></span>
+                            </span>
+                        </label>
+                        <input type="hidden" name="action" value="sell">
+                        <input type="submit" class="button" name="submit" value="Submit">
+                    </form>
+                </div>
             </div>
         </section>
         <section id="reviews">
             <h3>Reveiws</h3>
             <div>
-                <p>Coming soon!</p>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                <label> Rating: 
+                    <span>
+                        <select name="rate" value="<?php echo $rate;?>">
+                            <option value='1'>1</option>
+                            <option value='2'>2</option>
+                            <option value='3'>3</option>
+                            <option value='4'>4</option>
+                            <option value='5'>5</option>
+                        </select>
+                        <span class="error">* <?php echo $rateErr;?></span>
+                    </span>
+                </label>
+                <label> Review: 
+                    <span>
+                        <textarea name="textbox" value="<?php echo $textbox;?>"></textarea>
+                        <span class="error">* <?php echo $reivewErr;?></span>
+                    </span>
+                </label>
+                <input type="hidden" name="action" value="review">
+                <input type="submit" class="button" name="submit" value="Submit">
+            </form>
             </div>
-            <!--- Needs to be built out later -->
         </section>
 
     </main>
