becf51766ed914424fe9847d28a4cf4eb27e818d
Collin Blake <collinblake1@gmail.com>
2020-10-20 23:39:24 -0600

    Fixed callout

--- web/Assignments/DBUpdate/details.php
+++ web/Assignments/DBUpdate/details.php
@@ -71,7 +71,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     $success = $db->query("INSERT INTO reviews 
                         (game_id, rating, review_text)
                     VALUES 
-                        (". $ID .", ". $rate .", '". mysql_real_escape_string($textbox) ."');"))
+                        (". $ID .", ". $rate .", '". real_escape_string($textbox) ."');"))
                     {                                            
                         $rate = $textbox = "";
                     }
