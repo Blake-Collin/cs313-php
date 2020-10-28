b47ad4f6371105e39a1df0a1e76c9c5253ecb0c0
Collin Blake <collinblake1@gmail.com>
2020-10-27 20:14:34 -0600

    JS Added

--- web/Teach/07Teach/js/verify.js
+++ web/Teach/07Teach/js/verify.js
@@ -6,11 +6,13 @@ function checkUser(inputName) {
     var name = document.getElementById(inputName).value;
     if(!name.match(NAMEREGEX))
     {
-        document.getElementById(inputName + "Err").value = "* Username is inncorrect format must be Letters only!";
+        console.log("testfail");
+        document.getElementById(inputName + "Err").innerHTML = "* Username is inncorrect format must be Letters only!";
     }
     else
     {
-        document.getElementById(inputName + "Err").value = "*";
+        console.log("testsuccess");
+        document.getElementById(inputName + "Err").innerHTML = "*";
     }
 }
 
@@ -18,10 +20,12 @@ function checkPassword(inputPass) {
     var pass = document.getElementById(inputPass).value;
     if(!pass.match(PASSREGEX))
     {
-        document.getElementById(inputPass + "Err").value = "* Minimum eight characters, at least one uppercase letter, one lowercase letter, one number, and one special character is required!";
+        console.log("testfail");
+        document.getElementById(inputPass + "Err").innerHTML = "* Minimum eight characters, at least one uppercase letter, one lowercase letter, one number, and one special character is required!";
     }
     else
     {
-        document.getElementById(inputPass + "Err").value = "*";
+        console.log("testsuccess");
+        document.getElementById(inputPass + "Err").innerHTML = "*";
     }
 }
