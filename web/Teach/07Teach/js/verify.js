60c159c8797c1aa74ee395ffd91bdcec48fa18ba
Collin Blake <collinblake1@gmail.com>
2020-10-27 20:12:41 -0600

    JS Added

--- web/Teach/07Teach/js/verify.js
+++ web/Teach/07Teach/js/verify.js
@@ -4,7 +4,7 @@ var NAMEREGEX = "/^[a-zA-Z\-]+$/";
 
 function checkUser(inputName) {
     var name = document.getElementById(inputName).value;
-    if(!name.value.match(NAMEREGEX))
+    if(!name.match(NAMEREGEX))
     {
         document.getElementById(inputName + "Err").value = "* Username is inncorrect format must be Letters only!";
     }
@@ -16,7 +16,7 @@ function checkUser(inputName) {
 
 function checkPassword(inputPass) {
     var pass = document.getElementById(inputPass).value;
-    if(!pass.value.match(PASSREGEX))
+    if(!pass.match(PASSREGEX))
     {
         document.getElementById(inputPass + "Err").value = "* Minimum eight characters, at least one uppercase letter, one lowercase letter, one number, and one special character is required!";
     }
